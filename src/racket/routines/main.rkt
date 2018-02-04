0ed530a4c6b7b9cc6e5d16065f8f836724bb4d4e
Lucas Morales <lucas@lucasem.com>
2018-02-03 21:22:56 -0500

    add replace-k-with-n

--- src/racket/routines/main.rkt
+++ src/racket/routines/main.rkt
@@ -944,6 +944,23 @@
 )
 
 (let ()
+  (local-require "replace-k-with-n.rkt")
+  (set! h (make-hash))
+  (hash-set! h 'description description)
+  (hash-set! h 'is-parametric is-parametric)
+  (hash-set! h 'deps deps)
+  (hash-set! h 'validate validate)
+  (hash-set! h 'evaluate evaluate)
+  (hash-set! h 'generate generate)
+  (if is-parametric
+      (begin
+        (hash-set! h 'example-params example-params)
+        (hash-set! h 'validate-params validate-params))
+      (hash-set! h 'examples examples))
+  (hash-set! routines 'replace-k-with-n h)
+)
+
+(let ()
   (local-require "reverse.rkt")
   (set! h (make-hash))
   (hash-set! h 'description description)
